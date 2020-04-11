26463858b60246d1daa1cac42a397cd30af19a09
ALi Ip <alex.musa1988@gmail.com>
2020-04-11 14:47:29 -0500

    Updates

---
+++ precache-manifest.ffac7966237ac229f69babd1ba5e970f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115b84837f0ff7838e3a440ec0c47f88",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "5b084ab964b0f971f87d",
+    "url": "/myreact/static/css/main.993bc322.chunk.css"
+  },
+  {
+    "revision": "6dc77600ca84b07dda09",
+    "url": "/myreact/static/js/2.3208917d.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/myreact/static/js/2.3208917d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b084ab964b0f971f87d",
+    "url": "/myreact/static/js/main.f8af224a.chunk.js"
+  },
+  {
+    "revision": "593ea8bd118b9727791d",
+    "url": "/myreact/static/js/runtime-main.779dcf26.js"
+  },
+  {
+    "revision": "3122b5adb308534d629a950b7d4ada46",
+    "url": "/myreact/static/media/alex.3122b5ad.jpg"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/myreact/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/myreact/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/myreact/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/myreact/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/myreact/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "ed0debef40b4892f58ce73c9342d37d5",
+    "url": "/myreact/static/media/silver-and-black-laptop-computer-1229861.ed0debef.jpg"
+  },
+  {
+    "revision": "95777b9fa71df946e0db47b3543d911e",
+    "url": "/myreact/static/media/user.95777b9f.png"
+  }
+]);
